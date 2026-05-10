354658fc9c620d3ca114e0ff26964c1621bd3988
Shaan Satsangi <shaansatsangi@gmail.com>
2026-05-10 09:54:48 +0530

    feat: add CRM sales pipeline DAG and initial CI workflow configuration

--- dags/crm_sales_pipeline.py
+++ dags/crm_sales_pipeline.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
+
 from etl.extract import main as extract_main
 from etl.load_staging import main as load_main
 from etl.transform_warehouse import main as transform_main
@@ -7,34 +8,51 @@ from etl.quality_checks import main as quality_main
 
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG
+from airflow.sdk.definitions.taskgroup import TaskGroup
 
 PROJECT_ROOT = Path("/usr/local/airflow")
 
+default_args = {
+    "owner": "shaan",
+    "retries": 3,
+    "retry_delay": timedelta(minutes=1),
+}
+
 with DAG(
     dag_id="crm_sales_warehouse_pipeline",
+    default_args=default_args,
     start_date=datetime(2025, 1, 1),
     schedule="0 0 * * *",
     catchup=False,
     tags=["crm", "etl", "warehouse"],
 ) as dag:
-    extract_data = PythonOperator(
-    task_id="extract_data",
-    python_callable=extract_main,
-)
 
-load_staging = PythonOperator(
-    task_id="load_staging",
-    python_callable=load_main,
-)
+    with TaskGroup(group_id="extraction") as extraction_group:
 
-transform_warehouse = PythonOperator(
-    task_id="transform_warehouse",
-    python_callable=transform_main,
-)
+        extract_data = PythonOperator(
+            task_id="extract_data",
+            python_callable=extract_main,
+        )
 
-quality_checks = PythonOperator(
-    task_id="quality_checks",
-    python_callable=quality_main,
-)
+    with TaskGroup(group_id="loading") as loading_group:
 
-extract_data >> load_staging >> transform_warehouse >> quality_checks
+        load_staging = PythonOperator(
+            task_id="load_staging",
+            python_callable=load_main,
+        )
+
+    with TaskGroup(group_id="transformation") as transformation_group:
+
+        transform_warehouse = PythonOperator(
+            task_id="transform_warehouse",
+            python_callable=transform_main,
+        )
+
+    with TaskGroup(group_id="validation") as validation_group:
+
+        quality_checks = PythonOperator(
+            task_id="quality_checks",
+            python_callable=quality_main,
+        )
+
+    extraction_group >> loading_group >> transformation_group >> validation_group

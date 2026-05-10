e89745cca8ffe0925401f310412dbd52fcc748a4
Shaan Satsangi <shaansatsangi@gmail.com>
2026-05-10 09:27:07 +0530

    bash -> pythoon operators

--- dags/crm_sales_pipeline.py
+++ dags/crm_sales_pipeline.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from pathlib import Path
+from etl.extract import main as extract_main
+from etl.load_staging import main as load_main
+from etl.transform_warehouse import main as transform_main
+from etl.quality_checks import main as quality_main
 
-from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG
 
 PROJECT_ROOT = Path("/usr/local/airflow")
@@ -13,24 +17,24 @@ with DAG(
     catchup=False,
     tags=["crm", "etl", "warehouse"],
 ) as dag:
-    extract_data = BashOperator(
-        task_id="extract_data",
-        bash_command=f"cd {PROJECT_ROOT} && python -m etl.extract",
-    )
+    extract_data = PythonOperator(
+    task_id="extract_data",
+    python_callable=extract_main,
+)
 
-    load_staging = BashOperator(
-        task_id="load_staging",
-        bash_command=f"cd {PROJECT_ROOT} && python -m etl.load_staging",
-    )
+load_staging = PythonOperator(
+    task_id="load_staging",
+    python_callable=load_main,
+)
 
-    transform_warehouse = BashOperator(
-        task_id="transform_warehouse",
-        bash_command=f"cd {PROJECT_ROOT} && python -m etl.transform_warehouse",
-    )
+transform_warehouse = PythonOperator(
+    task_id="transform_warehouse",
+    python_callable=transform_main,
+)
 
-    quality_checks = BashOperator(
-        task_id="quality_checks",
-        bash_command=f"cd {PROJECT_ROOT} && python -m etl.quality_checks",
-    )
+quality_checks = PythonOperator(
+    task_id="quality_checks",
+    python_callable=quality_main,
+)
 
-    extract_data >> load_staging >> transform_warehouse >> quality_checks
+extract_data >> load_staging >> transform_warehouse >> quality_checks
